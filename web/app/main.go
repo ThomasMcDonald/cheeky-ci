3e91116b75eec88f887bc0b2b2cf877e6d0a655a
ThomasMcDonald <thomasmcdonald1996@gmail.com>
2026-01-25 14:35:46 +1000

    feat: postgres connection

--- web/app/main.go
+++ web/app/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
@@ -34,14 +38,58 @@ Get Updated Content
 	http.HandleFunc("/update", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 
-		_, err := fmt.Fprintln(w, "<p>test test test</p>")
+		conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Unable to connect to database %v\n", err)
+		}
+
+		defer conn.Close(context.Background())
+
+		name := "initial job"
+		var id string
+
+		err = conn.QueryRow(context.Background(), "INSERT INTO jobs (name) VALUES ($1) RETURNING id", name).Scan(&id)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "query error %v\n", err)
+		}
+
+		_, err = fmt.Fprintln(w, "<p>New row: %s</p>", id)
 		if err != nil {
 			panic(err)
 		}
 	})
-	fmt.Println("Server running at 8080")
 
-	err := http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/runner/register", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		_, err := fmt.Fprintln(w, "{}")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/runner/{id}/heartbeat", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		_, err := fmt.Fprintln(w, "{}")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("Server running at 5173")
+
+	err := http.ListenAndServe(":5173", nil)
 	if err != nil {
 		panic(err)
 	}
